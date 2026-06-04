3f81dd4b4d10b95f8f33abb8db94a7afbba75723
MuttonString <16234921893@qq.com>
2026-06-04 15:16:17 +0800

    package upd

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,7 +9,17 @@ mod webview_ver;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+    builder
         .plugin(
             tauri_plugin_log::Builder::default()
                 .targets([
